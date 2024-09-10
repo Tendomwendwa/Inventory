bdffc21f73a713b413714a7cedabe498bac2dc3a
Limoo Kiplimo <limoodrice@gmail.com>
2024-09-10 13:06:01 +0300

    fix: update project structure - limoo Todo - add css

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from .views import home_view
-from .views import login_view, register_view
+from .views import login_view, register_view, users_view, items_requests_view, items_view
 
 urlpatterns = [
     path('', home_view, name='home'),
     path('login/', login_view, name='login'),
     path('register/', register_view, name='register'),
+    path('users/', users_view, name='users'),
+    path('items/', items_view, name='items'),
+    path('item/requests/', items_requests_view, name='item-requests'),
 ]
